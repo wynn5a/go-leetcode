fbdab3004122b3a355b223aad1a199862495a155
wynn5a <winminy@163.com>
2021-12-15 10:59:01 +0800

    solution of 630 --add comment and clean code

--- pkg/solution630/course_schedule_3.go
+++ pkg/solution630/course_schedule_3.go
@@ -2,6 +2,7 @@ package solution630
 
 import (
 	"container/heap"
+	"fmt"
 	"sort"
 )
 
@@ -17,25 +18,33 @@ import (
 //Input: courses = [[3,2],[4,3]]
 //Output: 0
 func scheduleCourse(courses [][]int) int {
+	//sort by lastDay of course
 	sort.Slice(courses, func(i, j int) bool {
 		return courses[i][1] < courses[j][1]
 	})
 
+	fmt.Printf("sorted by last day: %v \n", courses)
+
 	h := &Heap{}
-	total := 0 // total time in heap
+	total := 0 // total time used for courses
 	for _, course := range courses {
+		//if we can take this course, push it's duration into heap,
+		//otherwise check and replace the longer course in heap
 		if t := course[0]; total+t <= course[1] {
 			total += t
 			heap.Push(h, t)
+			fmt.Printf("take the course: %v, now in heap: %v \n", course, h)
 		} else if h.Len() > 0 && t < h.IntSlice[0] {
 			total += t - h.IntSlice[0]
 			h.IntSlice[0] = t
 			heap.Fix(h, 0)
+			fmt.Printf("replace the course in heap with: %v \n", course)
 		}
 	}
 	return h.Len()
 }
 
+//Heap implement sort.Interface and Heap interface
 type Heap struct {
 	sort.IntSlice
 }
@@ -54,4 +63,3 @@ func (h *Heap) Pop() interface{} {
 	h.IntSlice = a[:len(a)-1]
 	return x
 }
-
