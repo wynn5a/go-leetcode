4d9f3d7e31edc6db04d506e21af2e290a4912815
wynn5a <winminy@163.com>
2021-12-17 10:05:43 +0800

    run project by testing

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # go-leetcode
 Leetcode solution in Golang
 
+# How to run
+
+You can check all solutions by testing
+
+```shell
+go test ./...
+```
+
+
 ## TODO
 
 * Add a README for each solution and explain it.
---
+++ pkg/solution/solution_test.go
@@ -0,0 +1,25 @@
+package solution
+
+import (
+	"testing"
+)
+
+func Test_21(t *testing.T) {
+	Run21()
+}
+
+func Test_26(t *testing.T) {
+	Run26()
+}
+
+func Test_189(t *testing.T) {
+	Run189()
+}
+
+func Test_300(t *testing.T) {
+	Run300()
+}
+
+func Test_807(t *testing.T) {
+	Run807()
+}
