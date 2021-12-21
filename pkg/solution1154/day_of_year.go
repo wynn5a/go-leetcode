815e41b7e052c7f701573b521ac0cb739ee27b6f
wynn5a <winminy@163.com>
2021-12-21 17:19:08 +0800

    solution of 1154 - optimization

--- pkg/solution1154/day_of_year.go
+++ pkg/solution1154/day_of_year.go
@@ -7,16 +7,15 @@ func dayOfYear(date string) int {
 	year, _ := strconv.Atoi(date[:4])
 	month, _ := strconv.Atoi(date[5:7])
 	day, _ := strconv.Atoi(date[8:])
-	var monthDay []int
+
 	days := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 	if year%400 == 0 || (year%4 == 0 && year%100 != 0) {
 		days[1]++
 	}
-	monthDay = append(monthDay, 0)
-	monthDay = append(monthDay, 0)
-	for i := 2; i <= 13; i++ {
-		monthDay = append(monthDay, monthDay[i-1]+days[i-2])
-	}
 
-	return day + monthDay[month]
+	ans := day
+	for _, d := range days[:month-1] {
+		ans += d
+	}
+	return ans
 }
