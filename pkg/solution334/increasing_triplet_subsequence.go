f9db8666353f5ca17b7ecb8ffe306b3e21fba078
wynn5a <winminy@163.com>
2022-01-12 11:25:15 +0800

    add solution for 334 -- optimization

--- pkg/solution334/increasing_triplet_subsequence.go
+++ pkg/solution334/increasing_triplet_subsequence.go
@@ -37,3 +37,23 @@ func max(a, b int) int {
 	}
 	return a
 }
+
+//贪心算法
+//func increasingTriplet(nums []int) bool {
+//	n := len(nums)
+//	if n < 3 {
+//		return false
+//	}
+//	first, second := nums[0], math.MaxInt32
+//	for i := 1; i < n; i++ {
+//		num := nums[i]
+//		if num > second {
+//			return true
+//		} else if num > first {
+//			second = num
+//		} else {
+//			first = num
+//		}
+//	}
+//	return false
+//}
