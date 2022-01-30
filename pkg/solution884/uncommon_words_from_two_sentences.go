3fdefe159a3659c57730af173aa60ade2e90c389
wynn5a <winminy@163.com>
2022-01-30 13:56:18 +0800

    add solution for 884

---
+++ pkg/solution884/uncommon_words_from_two_sentences.go
@@ -0,0 +1,25 @@
+package solution884
+
+import "strings"
+
+func uncommonFromSentences(s1 string, s2 string) []string {
+	freq := make(map[string]int)
+
+	insert := func(s string) {
+		words := strings.Split(s, " ")
+		for _, word := range words {
+			freq[word]++
+		}
+	}
+
+	insert(s1)
+	insert(s2)
+
+	ans := []string{}
+	for word, occ := range freq {
+		if occ == 1 {
+			ans = append(ans, word)
+		}
+	}
+	return ans
+}
---
+++ pkg/solution884/uncommon_words_from_two_sentences_test.go
@@ -0,0 +1,30 @@
+package solution884
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_case_1(t *testing.T) {
+	var (
+		s1       = "this apple is sweet"
+		s2       = "this apple is sour"
+		expected = []string{"sweet", "sour"}
+	)
+	actual := uncommonFromSentences(s1, s2)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("uncommonFromSentences(%s, %s) = %v; expected %v", s1, s2, actual, expected)
+	}
+}
+
+func Test_case_2(t *testing.T) {
+	var (
+		s1       = "apple apple"
+		s2       = "banana"
+		expected = []string{"banana"}
+	)
+	actual := uncommonFromSentences(s1, s2)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("uncommonFromSentences(%s, %s) = %v; expected %v", s1, s2, actual, expected)
+	}
+}
