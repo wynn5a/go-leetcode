9637c83f385b17d9ae092cbcada7bb44a5be774c
wynn5a <winminy@163.com>
2024-01-04 16:44:58 +0800

    add more test cases of 49

--- pkg/solution49/group-anagrams_test.go
+++ pkg/solution49/group-anagrams_test.go
@@ -6,32 +6,54 @@ import (
 )
 
 func TestCase(t *testing.T) {
-	input := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
-	expected := [][]string{
-		{"bat"},
-		{"nat", "tan"},
-		{"ate", "eat", "tea"},
-	}
-	actual := groupAnagrams(input)
-	if len(actual) != len(expected) {
-		t.Errorf("Expected %d groups, got %d", len(expected), len(actual))
+	cases := []struct {
+		input    []string
+		expected [][]string
+	}{
+		{
+			input: []string{"eat", "tea", "tan", "ate", "nat", "bat"},
+			expected: [][]string{
+				{"bat"},
+				{"nat", "tan"},
+				{"ate", "eat", "tea"},
+			},
+		},
+		{
+			input: []string{""},
+			expected: [][]string{
+				{""},
+			},
+		},
+		{
+			input: []string{"a"},
+			expected: [][]string{
+				{"a"},
+			},
+		},
 	}
 
-	for _, group := range actual {
-		checked := false
-		m := make(map[int]bool)
-		for i, strs := range expected {
-			if m[i] {
-				continue
-			}
-			if util.SameStringSlice(group, strs) {
-				m[i] = true
-				checked = true
-				break
-			}
+	for _, c := range cases {
+		actual := groupAnagrams(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Expected %d groups, got %d", len(c.expected), len(actual))
 		}
-		if !checked {
-			t.Errorf("Expected group %v to be one of %v", group, expected)
+
+		for _, group := range actual {
+			checked := false
+			m := make(map[int]bool)
+			for i, strs := range c.expected {
+				if m[i] {
+					continue
+				}
+				if util.SameStringSlice(group, strs) {
+					m[i] = true
+					checked = true
+					break
+				}
+			}
+			if !checked {
+				t.Errorf("Expected group %v to be one of %v", group, c.expected)
+			}
 		}
 	}
 }
